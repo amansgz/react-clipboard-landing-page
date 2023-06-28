37f1818683d5d6e153bf56b58a064b79d306ff44
amansgz <amansgz.ar@gmail.com>
2023-06-27 21:15:54 -0300

    Add responsive workflow section

---
+++ src/components/styles/Workflow.styled.jsx
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const StyledWorkflow = styled.section`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	padding: 3em 2em;
+	text-align: center;
+
+	div {
+		@media (min-width: 768px) {
+			display: flex;
+			flex-wrap: wrap;
+			justify-content: center;
+			gap: 1em;
+			margin: 3em 0;
+		}		
+	}	
+	
+`
+export const Title = styled.h2`
+	width: 60vw;
+`
+export const Text = styled.p`
+	width: 80vw;
+	max-width: 675px;
+	padding: 1em 0;
+`
+export const Feature = styled.article`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	gap: 1em;
+	text-align: center;
+
+`
+export const Icon = styled.picture`
+	width: 10vw;
+	max-width: 50px;
+	margin: 2em;
+`
+export const FeatureTitle = styled.h3`
+	padding-bottom: .5em;
+`
+export const FeatureText = styled.p`
+	width: 85vw;
+	max-width: 400px;
+`
